6d28ef7dc53eeef344677cb7bdfcdfb4fb53da71
thainguyencoffee <nguyennt11032004@gmail.com>
2024-09-06 13:44:48 +0700

    up

--- src/test/java/springjdbcdemo/SpringJdbcDemoApplicationTests.java
+++ src/test/java/springjdbcdemo/SpringJdbcDemoApplicationTests.java
@@ -2,12 +2,17 @@ package springjdbcdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("integration")
 class SpringJdbcDemoApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
